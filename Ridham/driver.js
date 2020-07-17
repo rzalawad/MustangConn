1ace155112c49c0fe703adb16d53f8900a20448b
Ridham Zalawadia <zalavadiaridham@gmail.com>
2020-07-17 08:43:39 -0700

    Email Auth Code Sender Added

---
+++ Ridham/driver.js
@@ -0,0 +1,34 @@
+var nodemailer = require('nodemailer');
+
+
+function generate_and_send_code(var email)
+{
+   var code = Math.floor(Math.random() * 888888) + 111111;
+
+   var transporter = nodemailer.createTransport({
+   service: 'gmail',
+   auth: {
+      user: 'MustangConnectAuthenticate@gmail.com',
+      pass: '307AssignmentRocks'
+      }
+   });
+
+   var mailOptions = {
+      from: 'MustangConnectAuthenticate@gmail.com',
+      to: email,
+      subject: 'MustangConnect Authentication Code',
+      text: `Here is your authentication code: ${code}
+
+      We hope you enjoy our site
+      Sincerely,
+      MustangConnect Team`
+   };
+
+   transporter.sendMail(mailOptions, function(error, info){
+   if (error) {
+      console.log(error);
+   } else {
+      console.log('Email sent: ' + info.response);
+   }
+   });
+}
